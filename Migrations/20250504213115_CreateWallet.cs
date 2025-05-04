31754f8b53bca9a70517697f9e8559155b7ef125
Lucas Xavier <lukasx1934@gmail.com>
2025-05-04 18:34:29 -0300

    feat: migration

---
+++ Migrations/20250504213115_CreateWallet.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateWallet : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Wallet_Users_OwnerId",
+                table: "Wallet");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Wallet",
+                table: "Wallet");
+
+            migrationBuilder.RenameTable(
+                name: "Wallet",
+                newName: "Wallets");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Wallet_OwnerId",
+                table: "Wallets",
+                newName: "IX_Wallets_OwnerId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Wallets",
+                table: "Wallets",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Wallets_Users_OwnerId",
+                table: "Wallets",
+                column: "OwnerId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Wallets_Users_OwnerId",
+                table: "Wallets");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Wallets",
+                table: "Wallets");
+
+            migrationBuilder.RenameTable(
+                name: "Wallets",
+                newName: "Wallet");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Wallets_OwnerId",
+                table: "Wallet",
+                newName: "IX_Wallet_OwnerId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Wallet",
+                table: "Wallet",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Wallet_Users_OwnerId",
+                table: "Wallet",
+                column: "OwnerId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
