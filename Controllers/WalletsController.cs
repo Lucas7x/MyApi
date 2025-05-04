7f551610fc888547e8b64f0ac5c49de68205916c
Lucas Xavier <lukasx1934@gmail.com>
2025-05-04 18:35:08 -0300

    feat: endpoint de buscar wallet

--- Controllers/WalletsController.cs
+++ Controllers/WalletsController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyApi.Controllers.DTOs;
 using MyApi.Data;
 
 namespace MyApi.Controllers
@@ -15,9 +16,47 @@ namespace MyApi.Controllers
         }
 
         [HttpGet]
-        public IActionResult Get()
+        public IActionResult Get([FromQuery] string? name, string? ownerName, int? ownerId)
         {
-            return Ok();
+            try
+            {
+                var wallets = _context.Wallets.AsQueryable();
+
+                if (!string.IsNullOrEmpty(name))
+                    wallets = wallets.Where(x => x.Name.Contains(name));
+
+                if (!string.IsNullOrEmpty(ownerName))
+                    wallets = wallets.Where(x => x.Owner.Name.Contains(ownerName));
+
+                if (ownerId != null)
+                    wallets = wallets.Where(x => x.OwnerId == ownerId);
+
+                var walletDtos = wallets.Select(x => new GetWalletDTO
+                {
+                    Id = x.Id,
+                    Name = x.Name,
+                    Description = x.Description,
+                    Balance = x.Balance,
+                    Income = x.Income,
+                    OwnerId = x.OwnerId,
+                    Owner = new GetPersonDTO
+                    {
+                        Id = x.Owner.Id,
+                        Name = x.Owner.Name,
+                        Email = x.Owner.Email,
+                        IsActive = x.Owner.IsActive
+                    }
+                }).ToList();
+
+                return Ok(new
+                {
+                    wallets = walletDtos
+                });
+            }
+            catch(Exception)
+            {
+                return BadRequest();
+            }
         }
     }
 }
