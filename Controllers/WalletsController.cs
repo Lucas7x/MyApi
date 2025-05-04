2152acf97452e21e888f3b1149b9ce130951a8e6
Lucas Xavier <lukasx1934@gmail.com>
2025-05-04 17:52:49 -0300

    feat: configurando WalletsController

--- Controllers/WalletsController.cs
+++ Controllers/WalletsController.cs
@@ -1,6 +1,23 @@
-﻿namespace MyApi.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using MyApi.Data;
+
+namespace MyApi.Controllers
 {
-    public class WalletsController
+    [ApiController]
+    [Route("[Controller]")]
+    public class WalletsController : ControllerBase
     {
+        private readonly DataContext _context;
+
+        public WalletsController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
     }
 }
