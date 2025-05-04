c0676dbd25257ca8d1e5a050612afefd3c4e6332
Lucas Xavier <lukasx1934@gmail.com>
2025-05-04 17:43:57 -0300

    feat: criando tabela wallet

---
+++ Models/Wallet.cs
@@ -0,0 +1,14 @@
+﻿namespace MyApi.Models
+{
+    public class Wallet
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Balance { get; set; }
+        public double Income { get; set; }
+
+        public int OwnerId { get; set; }
+        public Person Owner { get; set; }
+    }
+}
