bfe76cfeb2858ea1c4da00941d006f0a95b4252e
Lucas Xavier <lukasx1934@gmail.com>
2025-05-04 18:34:49 -0300

    feat: endpoint de buscar wallet

---
+++ Controllers/DTOs/GetWalletDTO.cs
@@ -0,0 +1,15 @@
+﻿using MyApi.Models;
+
+namespace MyApi.Controllers.DTOs
+{
+    public class GetWalletDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = "";
+        public string Description { get; set; } = "";
+        public double Balance { get; set; }
+        public double Income { get; set; }
+        public int? OwnerId { get; set; }
+        public GetPersonDTO? Owner { get; set; }
+    }
+}
