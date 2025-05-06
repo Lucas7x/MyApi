50e5798b7a1979f86138f8fac31cf4a73824670d
Lucas Xavier <lukasx1934@gmail.com>
2025-05-05 23:50:39 -0300

    feat: rename table user to person

---
+++ Migrations/20250506024309_RenameTableUser.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameTableUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Users",
+                table: "Users");
+
+            migrationBuilder.RenameTable(
+                name: "Users",
+                newName: "Persons");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Persons",
+                table: "Persons",
+                column: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Persons",
+                table: "Persons");
+
+            migrationBuilder.RenameTable(
+                name: "Persons",
+                newName: "Users");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Users",
+                table: "Users",
+                column: "Id");
+        }
+    }
+}
