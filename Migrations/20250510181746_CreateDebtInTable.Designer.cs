35cf70c6e94ae55008fa1605be832c211056a6a5
Lucas Xavier <lukasx1934@gmail.com>
2025-05-10 15:28:31 -0300

    feat: criando tabela de DebtIn

--- Migrations/20250510181746_CreateDebtInTable.Designer.cs
+++ Migrations/20250510181746_CreateDebtInTable.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MyApi.Data;
 
@@ -10,9 +11,11 @@ using MyApi.Data;
 namespace MyApi.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20250510181746_CreateDebtInTable")]
+    partial class CreateDebtInTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.0");
