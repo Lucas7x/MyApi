8f442f3c73d63eea5d4c8f06924ca49568228773
Lucas Xavier <lukasx1934@gmail.com>
2025-08-18 16:44:10 -0300

    feat: adicionando automapper para Person

---
+++ Mapping/PersonProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using MyApi.DTOs;
+using MyApi.Models;
+
+namespace MyApi.Mapping
+{
+    public class PersonProfile : Profile
+    {
+        public PersonProfile()
+        {
+            CreateMap<Person, PersonDTO>().ReverseMap();
+            CreateMap<Wallet, PersonWalletDTO>().ReverseMap();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -9,6 +9,9 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+// Configuring Automapper
+builder.Services.AddAutoMapper(x => x.AddMaps(typeof(Program).Assembly));
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
