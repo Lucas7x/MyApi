5495aac176336473d5753ef798d609d667143ca7
Lucas Xavier <lukasx1934@gmail.com>
2025-09-10 18:20:05 -0300

    feat: traduzindo mensagens de erro na API

--- API/MyWallet.Application/DTOs/PersonCreateDTO.cs
+++ API/MyWallet.Application/DTOs/PersonCreateDTO.cs
@@ -7,15 +7,15 @@ namespace MyWallet.Application.DTOs
         /// <summary>
         /// Nome completo do usuário.
         /// </summary>
-        [Required]
-        [StringLength(100, MinimumLength = 3)]
+        [Required(ErrorMessage = "O campo Nome é obrigatório.")]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "O campo Nome precisa ter entre 3 e 100 dígitos.")]
         public string Name { get; set; }
 
         /// <summary>
         /// Endereço de e-mail único.
         /// </summary>
-        [Required]
-        [EmailAddress]
+        [Required(ErrorMessage = "O campo E-mail é obrigatório.")]
+        [EmailAddress(ErrorMessage = "E-mail inválido.")]
         public string Email { get; set; }
 
     }
--- API/MyWallet.Application/DTOs/PersonUpdateDTO.cs
+++ API/MyWallet.Application/DTOs/PersonUpdateDTO.cs
@@ -7,13 +7,15 @@ namespace MyWallet.Application.DTOs
         /// <summary>
         /// Nome completo do usuário.
         /// </summary>
-        [StringLength(100, MinimumLength = 3)]
+        [Required(ErrorMessage = "O campo Nome é obrigatório.")]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "O campo Nome precisa ter entre 3 e 100 dígitos.")]
         public string? Name { get; set; }
 
         /// <summary>
         /// Endereço de e-mail único.
         /// </summary>
-        [EmailAddress]
+        [Required(ErrorMessage = "O campo E-mail é obrigatório.")]
+        [EmailAddress(ErrorMessage = "E-mail inválido.")]
         public string? Email { get; set; }
 
     }
