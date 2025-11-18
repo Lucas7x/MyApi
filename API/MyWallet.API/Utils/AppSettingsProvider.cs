b45e7326fc3fdb9db592230d261c0aad139dc4eb
Lucas Xavier <lukasx1934@gmail.com>
2025-11-18 14:46:49 -0300

    feat: serviços de appSettingsProvider e autenticação

---
+++ API/MyWallet.API/Utils/AppSettingsProvider.cs
@@ -0,0 +1,41 @@
+﻿using MyWallet.Application.Interfaces;
+
+namespace MyApi.Utils
+{
+    public class AppSettingsProvider : IAppSettingsProvider
+    {
+        private readonly IConfiguration _configuration;
+
+        public AppSettingsProvider(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string GetJwtAudience()
+        {
+            string? audience = _configuration.GetSection("Jwt")["Audience"];
+            if (string.IsNullOrEmpty(audience))
+                throw new ArgumentNullException(nameof(audience));
+
+            return audience;
+        }
+
+        public string GetJwtIssuer()
+        {
+            string? issuer = _configuration.GetSection("Jwt")["Issuer"];
+            if (string.IsNullOrEmpty(issuer))
+                throw new ArgumentNullException(nameof(issuer));
+
+            return issuer;
+        }
+
+        public string GetJwtSigningKey()
+        {
+            string? signingKey = _configuration.GetSection("Jwt")["IssuerSigningKey"];
+            if (string.IsNullOrEmpty(signingKey))
+                throw new ArgumentNullException(nameof(signingKey));
+
+            return signingKey;
+        }
+    }
+}
---
+++ API/MyWallet.Application/Service/AuthenticateService.cs
@@ -0,0 +1,70 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using MyWallet.Application.Interfaces;
+
+namespace MyWallet.Application.Service
+{
+    public class AuthenticateService : IAuthenticateService
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IAppSettingsProvider _appSettingsProvider;
+        private readonly IPasswordService _passwordService;
+
+        public AuthenticateService(IUserRepository userRepository, IAppSettingsProvider appSettingsProvider, IPasswordService passwordService)
+        {
+            _userRepository = userRepository;
+            _appSettingsProvider = appSettingsProvider;
+            _passwordService = passwordService;
+        }
+
+        public bool Authenticate(string email, string password)
+        {
+            var user = _userRepository.GetByEmail(email);
+            if (user == null)
+                return false;
+
+            if (_passwordService.VerifyPassword(password, user.PasswordHash, user.PasswordSalt))
+                return true;
+
+            return false;
+        }
+
+        public string GenerateToken(int id, string email)
+        {
+            var claims = new[]
+            {
+                new Claim("id", id.ToString()),
+                new Claim("email", email),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+
+            var privateKey = new SymmetricSecurityKey(Encoding.UTF8
+                .GetBytes(_appSettingsProvider.GetJwtSigningKey()));
+
+            var credentials = new SigningCredentials(privateKey, SecurityAlgorithms.HmacSha256);
+
+            var expiration = DateTime.Now.AddMinutes(10);
+
+            JwtSecurityToken token = new JwtSecurityToken(
+                issuer: _appSettingsProvider.GetJwtIssuer(),
+                audience: _appSettingsProvider.GetJwtAudience(),
+                claims: claims,
+                expires: expiration,
+                signingCredentials: credentials
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        public bool UserExists(string email)
+        {
+            var user = _userRepository.GetByEmail(email);
+            if (user == null)
+                return false;
+
+            return true;
+        }
+    }
+}
